4c96244f5f39cd4951ecce0867f4c61a816f24fe
rotcivegaf <victorfage@gmail.com>
2023-07-06 12:35:48 +0200

    Init

---
+++ test/t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/StdUtils.sol" ;
+
+contract BankTest is Test {
+    function setUp() public {
+    }
+
+    function test() public {
+
+    }
+}
